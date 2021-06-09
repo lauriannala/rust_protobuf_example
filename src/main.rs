1505c0b5a6cb459aaa90a8350ca6f0a1ec84e29c
Lauri Annala <lauri.annala@solita.fi>
2021-06-09 15:08:33 +0300

    Print byte size

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 mod lib;
 
+use std::mem;
+
 fn main() {
     let message = lib::create_message("Hello world!".to_string());
-    println!("Created message struct: {:?}", message);
+    println!("Created message struct: {:?}, byte_size: {}", message, mem::size_of_val(&message));
 
     let serialized_message = lib::serialize_message(&message);
-    println!("Serialized message: {:?}", serialized_message);
+    println!("Serialized message: {:?}, byte_size: {}", serialized_message, mem::size_of_val(&serialized_message));
 
     let deserialized_message = lib::deserialize_message(&serialized_message);
-    println!("Deserialized message: {:?}", deserialized_message);
+    println!("Deserialized message: {:?}, byte_size: {}", deserialized_message, mem::size_of_val(&deserialized_message));
 }
